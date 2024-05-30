695954735423368a6e414e5ba9fdf673cdc9159d
jlucas <117687632+l28j@users.noreply.github.com>
2024-05-30 11:29:11 +0100

    commit

--- populate1.sql
+++ populate1.sql
@@ -1,7 +1,7 @@
 -- Inserir Clínicas
 INSERT INTO clinica (nome, telefone, morada) VALUES
-('Clinica Lisboa', '217634253', 'Rua C, 1000-280 Lisboa'),
-('Clinica Oeiras', '213663534', 'Rua D, 1000-658 Oeiras'),
-('Clinica Cascais', '212627143', 'Rua K, 1000-529 Cascais'),
-('Clinica Sintra', '212652042', 'Rua K, 1000-149 Sintra'),
-('Clinica Amadora', '211071712', 'Rua E, 1000-632 Amadora');
+('Clinica Lisboa', '219930770', 'Rua L, 1000-895 Lisboa'),
+('Clinica Oeiras', '218027100', 'Rua J, 1000-236 Oeiras'),
+('Clinica Cascais', '215063026', 'Rua B, 1000-842 Cascais'),
+('Clinica Sintra', '217296546', 'Rua D, 1000-585 Sintra'),
+('Clinica Amadora', '216292890', 'Rua D, 1000-750 Amadora');
