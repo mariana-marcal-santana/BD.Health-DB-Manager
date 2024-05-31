83ea62536f4555e51bff0991b8340575142eeddf
Mariana Santana <mariana.marcal.santana@tecnico.ulisboa.pt>
2024-05-31 22:36:16 +0100

    del populates

--- populate1.sql
+++
@@ -1,7 +0,0 @@
--- Inserir Clínicas
-INSERT INTO clinica (nome, telefone, morada) VALUES
-('Clinica Lisboa', '213481226', 'Rua G, 1000-286 Lisboa'),
-('Clinica Oeiras', '210412325', 'Rua C, 1000-678 Oeiras'),
-('Clinica Cascais', '212404821', 'Rua L, 1000-115 Cascais'),
-('Clinica Sintra', '210617835', 'Rua O, 1000-479 Sintra'),
-('Clinica Amadora', '214451523', 'Rua B, 1000-354 Amadora');
